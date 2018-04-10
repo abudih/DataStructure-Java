fa19d30f9a980860746e2d6f8478513e7e5ca907
Andrew Budihardja <andrewbudihardja@gmail.com>
2018-04-10 14:04:45 -0700

    first commit

---
+++ SimView.java
@@ -0,0 +1,20 @@
+/**
+ * Basic interface for viewers of a SimModel. A SimView can be added to a
+ * SimModel, which will periodically notify the SimView so it can update its
+ * display.
+ * @author Andrew Budihardja
+ */
+
+public interface SimView {
+
+	/**
+	 * Notify this SimView so it can update its display of the SimModel.
+	 * 
+	 * @param m
+	 * 
+	 *            SimModel notifying this SimViewer
+	 */
+
+	public void notify(SimModel m);
+
+}
